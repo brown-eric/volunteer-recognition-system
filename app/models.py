03602a8347d4140df9ebd2d62f20df446889200d
brown-eric <brown.eric@ufl.edu>
2024-10-27 12:03:30 -0400

    Added the functionality of creating events and signing up for them. Made it specific to user types so only volunteers can sign up and only v-o can create them. Also created a leaderboard for volunteers so they can see who's got the most

--- app/models.py
+++ app/models.py
@@ -15,6 +15,20 @@ class HoursLog(db.Model):
     added_to = db.Column(db.String(255), nullable=False)  # The email of the user who received the hours
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)  # When the hours were added
 
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    created_by = db.Column(db.String(80), nullable=False)  # Stores the name or email of the creating organization
+    attendees = db.relationship('User', secondary='event_attendees', backref='events')
+
+
+event_attendees = db.Table('event_attendees',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
+)
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     name = db.Column(db.String(80), nullable=False)
@@ -26,8 +40,8 @@ class User(db.Model, UserMixin):
 
 
 
-    def __repr__(self):
-        return f'<User {self.name}>'
+def __repr__(self):
+    return f'<User {self.name}>'
 
 
 __all__ = [User, ]
