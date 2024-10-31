62f726541ee0a49fab523fca3d69f5b9dfac2b80
Sasha <s.watanabe@ufl.edu>
2024-10-30 22:39:31 -0400

    what I have for tests so far

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,47 @@
 import pytest_flask
-
+import random
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError as SQLAlchemyOperationalError
+from sqlalchemy.pool import StaticPool
 from app import create_app
 from app.models import db
 from app.models import User
 
 # from https://pytest-with-eric.com/api-testing/pytest-flask-postgresql-testing/#Use-an-in-memory-database-SQLite
+def pytest_addoption(parser):
+    parser.addoption(
+        "--dburl",  # For Postgres use "postgresql://user:password@localhost/dbname"
+        action="store",
+        default="sqlite:///:memory:",  # Default uses SQLite in-memory database
+        help="Database URL to use for tests.",
+    )
+
+
+@pytest.fixture(scope="session")
+def db_url(request):
+    """Fixture to retrieve the database URL."""
+    return request.config.getoption("--dburl")
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_sessionstart(session):
+    db_url = session.config.getoption("--dburl")
+    try:
+        # Attempt to create an engine and connect to the database.
+        engine = create_engine(
+            db_url,
+            poolclass=StaticPool,
+        )
+        connection = engine.connect()
+        connection.close()  # Close the connection right after a successful connect.
+        print("Using Database URL:", db_url)
+        print("Database connection successful.....")
+    except SQLAlchemyOperationalError as e:
+        print(f"Failed to connect to the database at {db_url}: {e}")
+        pytest.exit(
+            "Stopping tests because database connection could not be established."
+        )
 @pytest.fixture(scope="session")
 def app():
     """Session-wide test 'app' fixture."""
@@ -25,10 +61,21 @@ def app():
 
 # from https://flask.palletsprojects.com/en/3.0.x/testing/
 @pytest.fixture
-def test_client(app):
+def client(app):
     """Test client for the app."""
     return app.test_client()
 
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
+
+@pytest.fixture
+def user_payload():
+    suffix = random.randint(1, 100)
+    return {
+        "name": f"JohnDoe",
+        "email": f"john_{suffix}@doe.com",
+        "password": f"password{suffix}",
+        "confirm_password": f"password{suffix}",
+        "role": f"volunteer"
+    }
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,36 +1,57 @@
 from app.models import User
 from app.extensions import bcrypt
+import json
 
+def test_success_registration(client, user_payload):
+    """
+        GIVEN a Flask application configured for testing
+        WHEN a user correctly fills out registration form (POST)
+        THEN check that the form submission is successful and redirects to the home page.
+    """
+    headers = {
+        'content-type': 'application/json',
+    }
+    response = client.post(
+        "/register", data=json.dumps(user_payload), content_type="application/json"
+    ) # I don't know how to get this to not return 400
+    assert response.status_code == 201
+
+    response = client.get("/user/JohnDoe")
+    assert response.status_code == 200
+
+    read_response_json = json.loads(response.data)
+    print(read_response_json)
+    assert len(read_response_json) == 1
 # from https://testdriven.io/blog/flask-pytest/
-def test_nologin_redirects(test_client):
+def test_nologin_redirects(client):
     """
     GIVEN a Flask application configured for testing and a user is not logged in
     WHEN any page requiring a user to be logged in is requested (GET)
     THEN check that the response redirects to the login page.
     """
-    response = test_client.get('/') # home page
+    response = client.get('/') # home page
     assert response.status_code == 302
-    response = test_client.get('/rewards')
+    response = client.get('/rewards')
     assert response.status_code == 302
-    response = test_client.get('/edit_profile')
+    response = client.get('/edit_profile')
     assert response.status_code == 302
-    response = test_client.get('/add_hours')
+    response = client.get('/add_hours')
     assert response.status_code == 302
-    response = test_client.get('/remove_user')
+    response = client.get('/remove_user')
     assert response.status_code == 302
-    response = test_client.get('/view_database')
+    response = client.get('/view_database')
     assert response.status_code == 302
-    response = test_client.get('/logout')
+    response = client.get('/logout')
     assert response.status_code == 302
 
 # https://blog.miguelgrinberg.com/post/how-to-write-unit-tests-in-python-part-3-web-applications
-def test_registration_form(test_client):
+def test_register_form(client):
     """
     GIVEN a Flask application configured for testing
     WHEN the /register page is requested (GET)
     THEN check that the response is given and the registration form is given.
     """
-    response = test_client.get('/register')
+    response = client.get('/register')
     assert response.status_code == 200
     html = response.get_data(as_text=True)
     # make sure all the fields are included
@@ -41,60 +62,32 @@ def test_registration_form(test_client):
     assert 'name="role"' in html
     assert 'name="submit"' in html
 
-def test_login_form(test_client):
+def test_login_form(client):
     """
     GIVEN a Flask application configured for testing
     WHEN the /login page is requested (GET)
     THEN check that the response is given and the login form is given.
     """
-    response = test_client.get('/login')
+    response = client.get('/login')
     assert response.status_code == 200
     html = response.get_data(as_text=True)
     assert 'name="email"' in html
     assert 'name="password"' in html
     assert 'name="submit"' in html
 
-# TODO: NEED HELP
-def test_register_user(test_client):
-    """
-    GIVEN a Flask application configured for testing
-    WHEN a user correctly fills out registration form (POST)
-    THEN check that the form submission is successful and redirects to the home page.
-        """
-    response = test_client.post('/register', data={
-        'name': 'alice',
-        'email': 'alice@example.com',
-        'password': 'foo',
-        'confirm_password': 'foo',
-        'role': 'Admin' # How to input this since this is a select, not a field
-        # Also the above return 400, should we write an exception for this?
-        },
-        follow_redirects=True)
-    assert response.status_code == 200
-    assert response.request.path == '/'
-        #
-        # # login with new user
-        # response = self.client.post('/auth/login', data={
-        #     'username': 'alice',
-        #     'password': 'foo',
-        # }, follow_redirects=True)
-        # assert response.status_code == 200
-        # html = response.get_data(as_text=True)
-        # assert 'Hi, alice!' in html
-
 # NEED HELP. Sends 400 error
-def test_login_redirects(test_client):
+def test_successful_login(client):
     """
     GIVEN a Flask application configured for testing
-    WHEN a user correctly fills out registration form (POST)
+    WHEN a user correctly fills out login form (POST)
     THEN check that the form submission is successful and redirects to the home page.
     """
-    User(name='alice',email='alice@example.com',password="foo")
-    response = test_client.post('/login', data=dict(email='alice@example.com',password="foo"),follow_redirects=True
-        )
+    User(name='alice',email='alice@example.com',password="foo", role="volunteer")
+    response = client.post('/login', data=dict(email='alice@example.com',password="foo", remember_me=False),follow_redirects=True
+        ) # I don't know how to get this to not return 400
     assert response.status_code == 200
     assert response.request.path == '/'
-    response = test_client.get('/login')
-    assert response.status_code == 302
-    response = test_client.get('/register')
-    assert response.status_code == 302
+    # response = client.get('/login')
+    # assert response.status_code == 302
+    # response = client.get('/register')
+    # assert response.status_code == 302
